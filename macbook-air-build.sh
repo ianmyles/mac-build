fbc73ae241d3d8d682d3d61e57cbfbf42d3861f1
Ian Myles <ianmyles@macmini.localdomain>
2021-10-30 10:55:29 +0100

    Initial Commit

---
+++ macbook-air-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Running Install Script"
+# Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install dropbox
+brew install --cask iterm2
+brew install wget
+brew install git
+brew install firefox
+brew install pyenv
+pyenv install -v 3.9.5
+brew install notion
+brew install visual-studio-code
+brew install google-chrome
+brew install postman
+brew install balenaetcher
+brew install dbeaver-community
+brew install openssl readline sqlite3 xz zlib
+brew install 1password
+brew install protonvpn
